930140ea0a75fdc812da010a55e1e026b7ddf179
Piotr Mińkowski <piotr_minkowski@wp.pl>
2019-10-10 01:11:56 +0200

    newest version

--- caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
+++ caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
@@ -2,26 +2,26 @@ package pl.piomin.services.caller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@LoadBalancerClients({
-		@LoadBalancerClient(name = "callme-service", configuration = ClientConfiguration.class)
-})
-@EnableCaching
 public class CallerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallerApplication.class, args);
 	}
 
+	@LoadBalanced
 	@Bean
 	RestTemplate template() {
 		return new RestTemplate();
 	}
 
+//	@Bean
+//	@Primary
+//	public BlockingLoadBalancerClient loadBalancer(LoadBalancerClientFactory factory) {
+//		return new BlockingLoadBalancerClient(factory);
+//	}
 }
--- caller-service/src/main/java/pl/piomin/services/caller/CallerController.java
+++ caller-service/src/main/java/pl/piomin/services/caller/CallerController.java
@@ -2,10 +2,8 @@ package pl.piomin.services.caller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
-import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,16 +20,12 @@ public class CallerController {
 	Environment environment;
 	@Autowired
 	RestTemplate template;
-	@Autowired
-	LoadBalancerClientFactory clientFactory;
 
 	@GetMapping
 	public String call() {
-		RoundRobinLoadBalancer lb = clientFactory.getInstance("callme-service", RoundRobinLoadBalancer.class);
-		ServiceInstance instance = lb.choose().block().getServer();
-		String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/callme";
+		String url = "http://callme-service/callme";
 		String callmeResponse = template.getForObject(url, String.class);
-		LOGGER.info(": {}");
+		LOGGER.info("Response: {}", callmeResponse);
 		return "I'm Caller running on port " + environment.getProperty("local.server.port")
 				+ " calling-> " + callmeResponse;
 	}
